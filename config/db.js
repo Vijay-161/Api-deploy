822e43dad1ea6d1c0d4a003ac9a446f7b00d7f67
vijay-161 <singhvijay5866@gmail.com>
2024-02-13 11:19:23 +0545

    new to deploy

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.LOCAL_DATABASE_URI);
+
+    console.log(
+      `MongoDB connected to : ${conn.connection.host}`.white.underline.bold
+    );
+  } catch (error) {
+    console.error(`MongoDB connection error: ${error}`.red.underline.bold);
+  }
+};
+
+module.exports = connectDB;
+
+
+
+// LOCAL_DATABASE_URI='mongodb://127.0.0.1:27017/shoeStore'
+// LOCAL_DATABASE_URI = 'mongodb+srv://VijaySingh:X5LHHbDRBvkHLHgk@cluster0.sgflk54.mongodb.net/?retryWrites=true&w=majority'
